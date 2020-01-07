3e316fa32ab50c63e40b092a93bd8e7bc52d88ef
T.Hattori <farmer@TH-MacBookPro.local>
2020-01-07 22:39:12 +0900

    20200107working

--- Programming1/myothello_4619072/myothello.c
+++ Programming1/myothello_4619072/myothello.c
@@ -47,7 +47,14 @@ void myothello(int *step,        /* 現在のステップ数(初期値は0) */
 	printf( "Input cell (x y)\n" );
 	scanf( "%d %d", &x, &y );
 	x--; y--;  /* 配列番号が0番から始まるため． */
-	o->board[x][y] = color; /* 選んだマスに石を置く */
+
+	// 課題２　すでに石がある位置には石を新たに石を置けなくする。
+	if (o->board[x][y] != EMPTY) {
+		printf("すでに石が置かれています。 %d\n", *step);
+		*step -= 1;
+	} else {
+		o->board[x][y] = color; /* 選んだマスに石を置く */
+	}
 }
 
 
@@ -55,8 +62,16 @@ void myothello(int *step,        /* 現在のステップ数(初期値は0) */
    盤面のサイズを変えたい場合は 8 の部分を好きな数字に変更してください．*/
 int main (int argc, char **argv){
 	int i;
+	//課題７　盤面のサイズを実行時オプションで与える。
+	int board_size = 8;
+	for (i=1; i<argc; i++) {
+        if (strcmp(argv[i], "-n") == 0 && i+1 < argc) {
+            board_size = atoi(argv[++i]);
+        }
+    }
+
 	struct view view={512,512,}, *v=&view; /* おまじない (ウィンドウ) */
-	struct othello othello_instance={8,{2,2},{0,0},NULL}, *o=&othello_instance; /* 8はボードの列(行)数．*/
+	struct othello othello_instance={board_size,{2,2},{0,0},NULL}, *o=&othello_instance; /* 8はボードの列(行)数．*/
 
 	/* ボード作成(2次元配列の確保) */
 	o->board = (int**)malloc( sizeof(int*) * o->n );
